0f8162014d1d379b6500b381c6b9c5e162ff8f43
natan-alex <natancarmo321@gmail.com>
2022-10-05 15:50:16 -0300

    Just show command instead of entering git window in mapping

--- lua/neovim/mappings.lua
+++ lua/neovim/mappings.lua
@@ -48,4 +48,4 @@ map("n", "<Leader>qq", ":q<CR>", default_options)
 map("n", "<Leader>Q", ":qa<CR>", default_options)
 
 -- Fugitive mappings
-map("n", "<Leader>g", "<CMD>Git<CR>", default_options)
+map("n", "<Leader>g", ":Git", default_options)
