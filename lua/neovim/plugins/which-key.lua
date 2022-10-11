3ae414c12121c1f7f4d4e605c79b82fec42b7105
natan-alex <natancarmo321@gmail.com>
2022-10-11 19:29:02 -0300

    remove unnecessary functions and put mappings in which-key file

--- lua/neovim/plugins/which-key.lua
+++ lua/neovim/plugins/which-key.lua
@@ -26,57 +26,28 @@ if not was_telescope_module_found then
     return
 end
 
-local function find_files()
-    telescope.find_files()
-end
-
-local function recent_files()
-    telescope.oldfiles()
-end
-
-local function find_buffers()
-    telescope.buffers()
-end
-
-local function live_grep()
-    telescope.live_grep()
-end
-
-local function find_in_current_buffer()
-    local opt = {
-        sorting_strategy = "ascending",
-    }
-
-    telescope.current_buffer_fuzzy_find(opt)
-end
-
-local function git_status()
-    telescope.git_status()
-end
-
-local function git_commits()
-    telescope.git_commits()
-end
-
-local function git_branches()
-    telescope.git_branches()
-end
-
 which_key.register({
     ["<Leader>"] = {
         f = {
             name = "telescope files",
-            f = { find_files, "Telescope Find file" },
-            r = { recent_files, "Telescope Open recent file" },
-            g = { live_grep, "Telescope Live grep" },
-            b = { find_buffers, "Telescope List buffers" },
-            c = { find_in_current_buffer, "Telescope Find in current buffer" },
+            f = { telescope.find_files, "Telescope Find file" },
+            r = { telescope.oldfiles, "Telescope Open recent file" },
+            g = { telescope.live_grep, "Telescope Live grep" },
+            b = { telescope.buffers, "Telescope List buffers" },
+            d = { telescope.diagnostics, "Telescope Diagnostics" },
+            c = {
+                function()
+                    local opt = { sorting_strategy = "ascending" }
+                    telescope.current_buffer_fuzzy_find(opt)
+                end,
+                "Telescope Find in current buffer"
+            },
         },
         g = {
             name = "telescope git",
-            s = { git_status, "Telescope Git status" },
-            b = { git_branches, "Telescope Git branches" },
-            c = { git_commits, "Telescope Git commits" },
-        }
+            s = { telescope.git_status, "Telescope Git status" },
+            b = { telescope.git_branches, "Telescope Git branches" },
+            c = { telescope.git_commits, "Telescope Git commits" },
+        },
     }
 }, options)
