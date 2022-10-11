356155f5448fb95f246c6d9e3f7b30e983d7d4db
natan-alex <natancarmo321@gmail.com>
2022-10-11 08:33:03 -0300

    remove some configs that are default for telescope

--- lua/neovim/plugins/telescope.lua
+++ lua/neovim/plugins/telescope.lua
@@ -27,23 +27,12 @@ telescope.setup({
                 ["<ESC>"] = actions.close,
                 ["<C-c>"] = actions.close,
 
-                ["j"] = actions.move_selection_next,
-                ["k"] = actions.move_selection_previous,
-                ["<C-n>"] = actions.move_selection_next,
-                ["<C-p>"] = actions.move_selection_previous,
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
-
                 ["gg"] = actions.move_to_top,
                 ["G"] = actions.move_to_bottom,
                 ["M"] = actions.move_to_middle,
 
                 ["<C-u>"] = actions.preview_scrolling_up,
                 ["<C-d>"] = actions.preview_scrolling_down,
-
-                ["<C-x>"] = actions.select_horizontal,
-                ["<C-v>"] = actions.select_vertical,
-                ["<C-t>"] = actions.select_tab,
 
                 ["?"] = actions.which_key,
             },
