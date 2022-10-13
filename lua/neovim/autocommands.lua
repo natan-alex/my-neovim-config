fd2b809011a82560b4fc925e7858818e74b79dfc
natan-alex <natancarmo321@gmail.com>
2022-10-13 15:20:58 -0300

    add autocmd to format on save

--- lua/neovim/autocommands.lua
+++ lua/neovim/autocommands.lua
@@ -4,6 +4,7 @@ local create_autocommand = vim.api.nvim_create_autocmd
 local group_options = { clear = true }
 
 local indentation_group = create_autogroup("IndentationRelated", group_options)
+local after_saving_group = create_autogroup("AfterSavingGroup", group_options)
 
 create_autocommand("FileType", {
     pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "html" },
@@ -11,5 +12,13 @@ create_autocommand("FileType", {
     callback = function()
         vim.opt_local.shiftwidth = 2
         vim.opt_local.softtabstop = 2
+    end,
+})
+
+create_autocommand("BufWritePre", {
+    pattern = "*",
+    group = after_saving_group,
+    callback = function()
+vim.lsp.buf.format({ async = true })
     end,
 })
