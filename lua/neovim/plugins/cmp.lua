086ac530435ce94418bbb530c5b7b8b28db06fd0
natan-alex <natancarmo321@gmail.com>
2022-10-14 09:08:53 -0300

    make some changes into cmp configs

--- lua/neovim/plugins/cmp.lua
+++ lua/neovim/plugins/cmp.lua
@@ -4,78 +4,79 @@ if not was_cmp_module_found then
     return
 end
 
-local was_luasnip_module_found, luasnip = pcall(require, "luasnip")
+local was_lspkind_module_found, lspkind = pcall(require, "lspkind")
 
-if not was_luasnip_module_found then
+if not was_lspkind_module_found then
     return
 end
 
-local icons = {
-    Text = "",
-    Method = "m",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
-}
+lspkind.init()
 
-local vim_item_menu = {
-    nvim_lsp = "[LSP]",
-    luasnip = "[Snippet]",
-    buffer = "[Buffer]",
-    path = "[Path]",
-}
-
-local function format_completion_menu_entry(entry, vim_item)
-    vim_item.kind = string.format('%s', icons[vim_item.kind])
-    vim_item.menu = vim_item_menu[entry.source.name]
-    return vim_item
-end
-
-local function expand_snippets(args)
-    luasnip.lsp_expand(args.body)
-end
+-- local icons = {
+--     Text = "",
+--     Method = "m",
+--     Function = "",
+--     Constructor = "",
+--     Field = "",
+--     Variable = "",
+--     Class = "",
+--     Interface = "",
+--     Module = "",
+--     Property = "",
+--     Unit = "",
+--     Value = "",
+--     Enum = "",
+--     Keyword = "",
+--     Snippet = "",
+--     Color = "",
+--     File = "",
+--     Reference = "",
+--     Folder = "",
+--     EnumMember = "",
+--     Constant = "",
+--     Struct = "",
+--     Event = "",
+--     Operator = "",
+--     TypeParameter = "",
+-- }
+--
 
 cmp.setup {
-    snippet = { expand = expand_snippets },
+    snippet = {
+        expand = function(args)
+            local was_luasnip_module_found, luasnip = pcall(require, "luasnip")
+
+            if not was_luasnip_module_found then
+                return
+            end
+
+            luasnip.lsp_expand(args.body)
+        end
+    },
     formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = format_completion_menu_entry,
+        format = lspkind.cmp_format({
+            with_text = true,
+            menu = {
+                nvim_lsp = "[LSP]",
+                luasnip = "[Snippet]",
+                buffer = "[Buffer]",
+                path = "[Path]",
+                cmdline = "[Cmdline]",
+            },
+        })
     },
     sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
-    },
-    confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
+        { name = "cmdline" },
     },
     window = {
         documentation = true,
     },
     experimental = {
-        ghost_text = false,
+        ghost_text = true,
         native_menu = false,
     },
     mapping = {
@@ -83,13 +84,16 @@ cmp.setup {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        ["<Tab>"] = cmp.mapping.confirm { select = true },
-        ["<C-e>"] = cmp.mapping {
+        ["<C-b>"] = cmp.mapping.scroll_docs(-5),
+        ["<C-f>"] = cmp.mapping.scroll_docs(5),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<Tab>"] = cmp.mapping.confirm({
+            select = true,
+            behavior = cmp.ConfirmBehavior.Insert,
+        }),
+        ["<C-e>"] = cmp.mapping({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
-        },
+        }),
     },
 }
