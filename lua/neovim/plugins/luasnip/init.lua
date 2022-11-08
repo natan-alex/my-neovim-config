4238f8014fb19c10735d25dcd9ba61e9a2dfe2ac
natan-alex <natancarmo321@gmail.com>
2022-11-08 15:26:20 -0300

    add folder for luasnip and create file for each language

--- lua/neovim/plugins/luasnip/init.lua
+++ lua/neovim/plugins/luasnip/init.lua
@@ -35,16 +35,5 @@ require("luasnip.loaders.from_vscode").lazy_load()
 
 -- CUSTOM SNIPPETS
 
-local create_snippet = luasnip.s
-local format = require("luasnip.extras.fmt").fmt
-local insert_node = luasnip.insert_node
--- local function_node = luasnip.function_node
--- local repeat_node = require("luasnip.extras").rep
-
-luasnip.add_snippets("cs", {
-    create_snippet("cws", format([[Console.WriteLine("{}");]], { insert_node(1) })),
-})
-
-luasnip.add_snippets("java", {
-    create_snippet("sout", format([[System.out.println("{}");]], { insert_node(1) })),
-})
+require("neovim.plugins.luasnip.cs-snippets")
+require("neovim.plugins.luasnip.java-snippets")
