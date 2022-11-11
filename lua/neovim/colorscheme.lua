ff6bdec4b43935862f17238bbfecc082e0ab3049
natan-alex <natancarmo321@gmail.com>
2022-11-11 14:05:53 -0300

    remove vscode colorscheme and just use onedark

--- lua/neovim/colorscheme.lua
+++ lua/neovim/colorscheme.lua
@@ -1,11 +1,1 @@
-require("neovim.plugins.colorschemes.vscode")
-
-local colorschemes = { "vscode", "onedark" }
-
-for _, colorscheme in ipairs(colorschemes) do
-    local could_apply_colorscheme = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
-    if could_apply_colorscheme then
-        break
-    end
-end
+pcall(vim.cmd, "colorscheme onedark")
