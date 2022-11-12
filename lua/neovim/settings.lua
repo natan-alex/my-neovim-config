165b7bc940e29ffe3841fa618dc4c39885c88397
natan-alex <natancarmo321@gmail.com>
2022-11-12 09:41:00 -0300

    use opt instead of o, bo, wo

--- lua/neovim/settings.lua
+++ lua/neovim/settings.lua
@@ -1,71 +1,65 @@
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-vim.o.hlsearch = false
-vim.o.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
-vim.bo.softtabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
-vim.o.smarttab = true
+vim.opt.wrap = true
 
-vim.o.wrap = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.o.hidden = true
+vim.opt.termguicolors = true
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.undofile = true -- enable persistent undo
 
-vim.o.termguicolors = true
+vim.opt.cursorline = true -- highlight the current line
 
-vim.o.undofile = true -- enable persistent undo
+vim.opt.mouse = "a" -- use mouse for selections and scroll
 
-vim.o.cursorline = true -- highlight the current line
+vim.opt.backup = false -- dont create a backup file
 
-vim.o.mouse = "a" -- use mouse for selections and scroll
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
 
-vim.o.backup = false -- dont create a backup file
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
-vim.o.cmdheight = 2 -- more space in the neovim command line for displaying messages
-vim.o.fileencoding = "utf-8" -- the encoding written to a file
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
 
-vim.bo.smartindent = true
-vim.bo.autoindent = true
+vim.opt.swapfile = false -- creates a swapfile
 
-vim.o.splitbelow = true -- force all horizontal splits to go below current window
-vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.scrolloff = 2
+vim.opt.sidescrolloff = 2
 
-vim.o.swapfile = false -- creates a swapfile
+vim.opt.lazyredraw = true
 
-vim.o.scrolloff = 2
-vim.o.sidescrolloff = 2
+vim.opt.timeoutlen = 500
 
-vim.o.lazyredraw = true
-
-vim.o.timeoutlen = 500
-
-vim.o.showmatch = true
-
-vim.o.backspace = "indent,eol,start",
+vim.opt.backspace = "indent,eol,start"
 
 
 -- Clipboard related
-vim.cmd([[set clipboard+=unnamedplus]])
-
-local winyank = "win32yank.exe"
-
-if vim.fn.executable(winyank) then
-    vim.g.clipboard = {
-        name = "win32yank-wsl",
-        copy = {
-            ["+"] = winyank .. " -i --crlf",
-            ["*"] = winyank .. " -i --crlf",
-        },
-        paste = {
-            ["+"] = winyank .. " -o --lf",
-            ["*"] = winyank .. " -o --lf",
-        },
-        ["cache_enabled"] = 0,
-    }
-end
+-- vim.cmd([[set clipboard+=unnamedplus]])
+--
+-- local winyank = "win32yank.exe"
+--
+-- if vim.fn.executable(winyank) then
+--     vim.g.clipboard = {
+--         name = "win32yank-wsl",
+--         copy = {
+--             ["+"] = winyank .. " -i --crlf",
+--             ["*"] = winyank .. " -i --crlf",
+--         },
+--         paste = {
+--             ["+"] = winyank .. " -o --lf",
+--             ["*"] = winyank .. " -o --lf",
+--         },
+--         ["cache_enabled"] = 0,
+--     }
+-- end
