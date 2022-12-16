ec91a6dac6a5eab82514be01bd004adfd9d8fb3e
natan-alex <natancarmo321@gmail.com>
2022-12-16 18:37:00 -0300

    change trouble mappings

--- lua/neovim/plugins/trouble.lua
+++ lua/neovim/plugins/trouble.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 local mapping_options = { noremap = true, silent = true }
 
 map("n", "gR", "<CMD>TroubleToggle lsp_references<CR>", mapping_options)
-map("n", "<leader>xx", "<CMD>TroubleToggle<CR>", mapping_options)
-map("n", "<leader>xl", "<CMD>TroubleToggle loclist<CR>", mapping_options)
-map("n", "<leader>xq", "<CMD>TroubleToggle quickfix<CR>", mapping_options)
-map("n", "<leader>xd", "<CMD>TroubleToggle document_diagnostics<CR>", mapping_options)
+map("n", "<leader>dl", "<CMD>TroubleToggle loclist<CR>", mapping_options)
+map("n", "<leader>dq", "<CMD>TroubleToggle quickfix<CR>", mapping_options)
+map("n", "<leader>dd", "<CMD>TroubleToggle document_diagnostics<CR>", mapping_options)
+map("n", "<leader>do", "<CMD>TroubleToggle<CR>", mapping_options)
