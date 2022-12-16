28244353c915249ad99ca057d31eb18e881bbb13
natan-alex <natancarmo321@gmail.com>
2022-12-16 18:31:54 -0300

    change mappings and keys in lspsaga (much better :))

--- lua/neovim/plugins/lspsaga.lua
+++ lua/neovim/plugins/lspsaga.lua
@@ -1,11 +1,21 @@
-require("lspsaga").init_lsp_saga()
+require("lspsaga").init_lsp_saga({
+    definition_action_keys = {
+        edit = "<Space>o",
+        vsplit = "<Space>v",
+        split = "<Space>i",
+        tabe = "<Space>t",
+        quit = "q",
+    },
+    rename_in_select = false,
+    finder_request_timeout = 3000,
+})
 
 local map = vim.keymap.set
 local mapping_options = { noremap = true, silent = true }
 
 map("n", "K", "<CMD>Lspsaga hover_doc<CR>", mapping_options)
 map("n", "gr", "<CMD>Lspsaga lsp_finder<CR>", mapping_options)
-map("n", "gD", "<CMD>Lspsaga peek_definition<CR>", mapping_options)
+map("n", "gd", "<CMD>Lspsaga peek_definition<CR>", mapping_options)
 map("n", "[d", "<CMD>Lspsaga diagnostic_jump_prev<CR>", mapping_options)
 map("n", "]d", "<CMD>Lspsaga diagnostic_jump_next<CR>", mapping_options)
 map("n", "<F2>", "<CMD>Lspsaga rename<CR>", mapping_options)
