4731c59983f35657c0b0ae904dd615367809fc53
natan-alex <natancarmo321@gmail.com>
2022-12-16 21:22:56 -0300

    mix mappings for lsp in lspsaga and in lspconfig

--- lua/neovim/plugins/lspsaga.lua
+++ lua/neovim/plugins/lspsaga.lua
@@ -13,13 +13,12 @@ require("lspsaga").init_lsp_saga({
 local map = vim.keymap.set
 local mapping_options = { noremap = true, silent = true }
 
+map("n", "<F2>", "<CMD>Lspsaga rename<CR>", mapping_options)
+map("n", "<Leader>rn", "<CMD>Lspsaga rename<CR>", mapping_options)
+map("n", "<Leader>ca", "<CMD>Lspsaga code_action<CR>", mapping_options)
+map("n", "<Leader>cd", "<CMD>Lspsaga show_line_diagnostics<CR>", mapping_options)
 map("n", "K", "<CMD>Lspsaga hover_doc<CR>", mapping_options)
-map("n", "gr", "<CMD>Lspsaga lsp_finder<CR>", mapping_options)
-map("n", "gd", "<CMD>Lspsaga peek_definition<CR>", mapping_options)
 map("n", "[d", "<CMD>Lspsaga diagnostic_jump_prev<CR>", mapping_options)
 map("n", "]d", "<CMD>Lspsaga diagnostic_jump_next<CR>", mapping_options)
-map("n", "<F2>", "<CMD>Lspsaga rename<CR>", mapping_options)
-map("n", "<Leader>ca", "<CMD>Lspsaga code_action<CR>", mapping_options)
-map("n", "<Leader>rn", "<CMD>Lspsaga rename<CR>", mapping_options)
-map("n", "<Leader>cd", "<CMD>Lspsaga show_line_diagnostics<CR>", mapping_options)
-map("n", "<Leader>o", "<CMD>Lspsaga outline<CR>", mapping_options)
+map("n", "gr", "<CMD>Lspsaga lsp_finder<CR>", mapping_options)
+map("n", "gD", "<CMD>Lspsaga peek_definition<CR>", mapping_options)
--- lua/neovim/plugins/mason-lspconfig.lua
+++ lua/neovim/plugins/mason-lspconfig.lua
@@ -7,31 +7,29 @@ local function on_lsp_attach(_, buffer_number)
     local map = vim.keymap.set
     local mapping_options = { noremap = true, silent = true, buffer = buffer_number }
 
+    -- map("n", "<Leader>rn", vim.lsp.buf.rename, options)
+    -- map("n", "<Leader>ca", vim.lsp.buf.code_action, options)
     -- map("n", "[d", vim.diagnostic.goto_prev, options)
     -- map("n", "]d", vim.diagnostic.goto_next, options)
     -- map("n", "K", vim.lsp.buf.hover, options)
     -- map("n", "<C-s>", vim.lsp.buf.signature_help, options)
-    -- map("n", "gD", vim.lsp.buf.declaration, options)
-    -- map("n", "gd", vim.lsp.buf.definition, options)
-    -- map("n", "gi", vim.lsp.buf.implementation, options)
     -- map("n", "gr", vim.lsp.buf.references, options)
-    -- map("n", "<Leader>rn", vim.lsp.buf.rename, options)
-    -- map("n", "<Leader>ca", vim.lsp.buf.code_action, options)
-    -- map("n", "<Leader>ll", vim.diagnostic.setloclist, options)
-    -- map("n", "<Leader>ql", vim.diagnostic.setqflist, options)
-    map("n", "<C-F>", vim.lsp.buf.format, mapping_options)
+    -- map("n", "gD", vim.lsp.buf.declaration, options)
+    map("n", "gd", vim.lsp.buf.definition, options)
+    map("n", "gi", vim.lsp.buf.implementation, options)
+    map("n", "<C-f>", vim.lsp.buf.format, mapping_options)
 end
 
 mason_lspconfig.setup()
 
 mason_lspconfig.setup_handlers({
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
     function(server_name)
         lspconfig[server_name].setup {
-	    on_attach = on_lsp_attach,
-	    capabilities = lsp_capabilities,
+            on_attach = on_lsp_attach,
+            capabilities = lsp_capabilities,
         }
     end,
 })
