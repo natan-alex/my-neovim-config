f121966a41dd305d5efa5e7fa1f95a1a75a6bf8c
Natan Alexandre <natancarmo321@gmail.com>
2022-12-18 10:13:30 -0300

    add reload config and highlight on yank autocommands

--- lua/neovim/autocommands.lua
+++ lua/neovim/autocommands.lua
@@ -5,6 +5,10 @@ local group_options = { clear = true }
 
 local indentation_group = create_autogroup("IndentationRelated", group_options)
 
+local packer_group = create_autogroup("Packer", group_options)
+
+local highlight_group = create_autogroup("YankHighlight", group_options)
+
 create_autocommand("FileType", {
     pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "css", "html" },
     group = indentation_group,
@@ -12,4 +16,21 @@ create_autocommand("FileType", {
         vim.opt_local.shiftwidth = 2
         vim.opt_local.softtabstop = 2
     end,
+})
+
+-- Automatically source and re-compile packer whenever init.lua is changed
+create_autocommand("BufWritePost", {
+    command = "source <afile> | PackerCompile",
+    group = packer_group,
+    pattern = vim.fn.expand("$MYVIMRC"),
+})
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+create_autocommand("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
 })
