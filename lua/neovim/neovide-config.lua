fc2170feece68b4c943d40157c8a292cae382e73
natan-alexandre <94197532+natan-alexandre@users.noreply.github.com>
2022-12-26 15:01:21 -0300

    correct copy mapping in neovide-config

--- lua/neovim/neovide-config.lua
+++ lua/neovim/neovide-config.lua
@@ -5,3 +5,7 @@ end
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_remember_window_size = true
 vim.g.neovide_cursor_antialiasing = true
+
+
+vim.keymap.set({ "v", "n" }, "<C-S-c>", [["+y]], { noremap = true, silent = true })
+vim.keymap.set({ "i", "n" }, "<C-S-v>", [[<C-r>+]], { noremap = true, silent = true })
