3e06bcb67ca130763020256e5833ad393dc45b28
natan-alex <natancarmo321@gmail.com>
2022-12-31 09:55:36 -0300

    change format doc and telescope navigation mappings

--- lua/my-config/plugins/telescope.lua
+++ lua/my-config/plugins/telescope.lua
@@ -8,11 +8,11 @@ telescope.setup({
         selection_caret = "»  ",
         mappings = {
             i = {
-                ["<C-j>"] = actions.move_selection_next,
-                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-n>"] = actions.move_selection_next,
+                ["<C-p>"] = actions.move_selection_previous,
 
-                ["<C-n>"] = actions.cycle_history_next,
-                ["<C-p>"] = actions.cycle_history_prev,
+                ["<C-j>"] = actions.cycle_history_next,
+                ["<C-k>"] = actions.cycle_history_prev,
 
                 ["<C-x>"] = actions.select_horizontal,
                 ["<C-v>"] = actions.select_vertical,
