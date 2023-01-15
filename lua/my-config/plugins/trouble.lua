12e901f057cd210d45222fd1c580edac5c4debe7
Natan Alexandre <natancarmo321@gmail.com>
2023-01-15 17:57:02 -0300

    change trouble mappings

--- lua/my-config/plugins/trouble.lua
+++ lua/my-config/plugins/trouble.lua
@@ -2,7 +2,7 @@ local map = require("my-config.utils.mappings").map
 local mapping_options = { silent = true, noremap = true, }
 
 map("n", "gR"        , "<CMD>TroubleToggle lsp_references<CR>",       mapping_options, "Trouble lsp references")
-map("n", "<Leader>xl", "<CMD>TroubleToggle loclist<CR>",              mapping_options, "Trouble loclist")
-map("n", "<Leader>xq", "<CMD>TroubleToggle quickfix<CR>",             mapping_options, "Trouble quickfix")
-map("n", "<Leader>xd", "<CMD>TroubleToggle document_diagnostics<CR>", mapping_options, "Trouble document diagnostics")
-map("n", "<Leader>xo", "<CMD>TroubleToggle<CR>",                      mapping_options, "Trouble toggle")
+map("n", "<Leader>dl", "<CMD>TroubleToggle loclist<CR>",              mapping_options, "Trouble loclist")
+map("n", "<Leader>dq", "<CMD>TroubleToggle quickfix<CR>",             mapping_options, "Trouble quickfix")
+map("n", "<Leader>dd", "<CMD>TroubleToggle document_diagnostics<CR>", mapping_options, "Trouble document diagnostics")
+map("n", "<Leader>do", "<CMD>TroubleToggle<CR>",                      mapping_options, "Trouble toggle")
