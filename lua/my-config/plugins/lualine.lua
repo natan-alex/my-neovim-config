aa926353471bafb8835b803b22991a1566ddd272
natan-alex <natancarmo321@gmail.com>
2023-04-29 08:44:03 -0300

    remove session name from lualine

--- lua/my-config/plugins/lualine.lua
+++ lua/my-config/plugins/lualine.lua
@@ -5,11 +5,6 @@ function M.setup_with_theme(theme)
         options = {
             icons_enabled = true,
             theme = theme
-        },
-        sections = {
-            lualine_c = {
-                require("auto-session").current_session_name
-            }
         }
     })
 end
