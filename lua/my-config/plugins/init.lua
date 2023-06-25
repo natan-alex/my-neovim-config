c8a2b4f02b9adea2aed8921e1baa664cf0ed10d5
natan-alex <natancarmo321@gmail.com>
2023-06-25 16:21:10 -0300

    add flit plugin

--- lua/my-config/plugins/init.lua
+++ lua/my-config/plugins/init.lua
@@ -144,6 +144,11 @@ local plugins = {
         config = function() require("leap").add_default_mappings() end,
     },
 
+    {
+        "ggandor/flit.nvim",
+        config = function() require("flit").setup() end,
+    },
+
     -- Nice indentation lines
     {
         "lukas-reineke/indent-blankline.nvim",
