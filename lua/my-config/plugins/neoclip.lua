868382f22ff5797fab0750c4e9b98c103c4d77e2
natan-alexandre <94197532+natan-alexandre@users.noreply.github.com>
2023-07-13 10:14:32 -0300

    remove unnused plugins

--- lua/my-config/plugins/neoclip.lua
+++
@@ -1,7 +0,0 @@
-require("neoclip").setup()
-require("telescope").load_extension("neoclip")
-
-local map = require("my-config.utils.mappings").map
-local mapping_options = { silent = true, noremap = true, }
-
-map("n", "<Leader>fn", "<CMD>Telescope neoclip<CR>", mapping_options, "Neoclip telescope extension")
