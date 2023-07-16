ada57dc045bb2eb9c6de50eddaea8519481fba3f
natan-alex <natancarmo321@gmail.com>
2023-07-16 12:02:24 -0300

    add option to use telescope and nowait in mapping options

--- lua/my-config/plugins/neogit.lua
+++ lua/my-config/plugins/neogit.lua
@@ -2,12 +2,13 @@ local neogit = require("neogit")
 
 neogit.setup {
     disable_signs = true,
+    use_telescope = true,
     integrations = {
         diffview = true
     },
 }
 
 local map = require("my-config.utils.mappings").map
-local mapping_options = { silent = true, noremap = true, }
+local mapping_options = { silent = true, noremap = true, nowait = true }
 
 map("n", "<Leader>gg", "<CMD>Neogit<CR>", mapping_options, "Toggle neogit")
