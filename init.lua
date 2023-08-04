4cfa29f171e34c4fdae7853a53caa7275004cfff
natan-alex <natancarmo321@gmail.com>
2023-08-03 23:17:21 -0300

    use paths join

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+local paths = require("my-config.utils.paths")
+
+local my_config_path = paths.join(vim.fn.stdpath("config"), "lua", "my-config")
+
 local core_config_files = {
     "globals.lua", -- some global settings
     "settings.vim", -- setting options in nvim
@@ -8,11 +12,12 @@ local core_config_files = {
 
 -- source all the core config files
 for _, file_name in ipairs(core_config_files) do
-    if vim.endswith(file_name, '.vim') then
-        local path = string.format("%s/lua/my-config/%s", vim.fn.stdpath("config"), file_name)
+    if vim.endswith(file_name, ".vim") then
+        local path = paths.join(my_config_path, file_name)
         vim.cmd("source " .. path)
     else
-        local module, _ = "my-config." .. string.gsub(file_name, "%.lua", "")
+        local file_name_without_extension = string.gsub(file_name, "%.lua", "")
+        local module, _ = "my-config." .. file_name_without_extension
         package.loaded[module] = nil
         require(module)
     end
