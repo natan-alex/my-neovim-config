60c1f13e31816fc710e3fd841931c666c5a5097e
natan-alex <natancarmo321@gmail.com>
2023-09-05 08:36:02 -0300

    change leap mappings

--- lua/my-config/plugins/leap.lua
+++ lua/my-config/plugins/leap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set({ "n", "x", "o" }, "sf", "<Plug>(leap-forward-to)")
-vim.keymap.set({ "n", "x", "o" }, "sb", "<Plug>(leap-backward-to)")
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
+vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward-to)")
