acd43ff2aff54c5a753114f7037f7ecf283f28d3
natan-alex <natancarmo321@gmail.com>
2023-09-21 22:45:48 -0300

    use settings in lua and make init simpler

--- init.lua
+++ init.lua
@@ -1,24 +1,14 @@
-local paths = require("my-config.utils.paths")
-
-local my_config_path = paths.join(vim.fn.stdpath("config"), "lua", "my-config")
-
 local core_config_files = {
-    "globals.lua", -- some global settings
-    "settings.vim", -- setting options in nvim
-    "mappings", -- all the user-defined mappings
-    "plugins",    -- all the plugins installed and their configurations
+    "globals",      -- some global settings
+    "settings",     -- setting options in nvim
+    "mappings",     -- all the user-defined mappings
+    "plugins",      -- all the plugins installed and their configurations
     "autocommands", -- various autocommands
 }
 
 -- source all the core config files
 for _, file_name in ipairs(core_config_files) do
-    if vim.endswith(file_name, ".vim") then
-        local path = paths.join(my_config_path, file_name)
-        vim.cmd("source " .. path)
-    else
-        local file_name_without_extension = string.gsub(file_name, "%.lua", "")
-        local module = "my-config." .. file_name_without_extension
-        package.loaded[module] = nil
-        require(module)
-    end
+    local module = "my-config." .. file_name
+    package.loaded[module] = nil
+    require(module)
 end
---
+++ lua/my-config/settings.lua
@@ -0,0 +1,135 @@
+-- Some settings were inspired in https://github.com/jdhao/nvim-config
+
+-- Align indent to next multiple value of shiftwidth
+vim.opt.shiftround     = true
+
+-- Highlight current line
+vim.opt.cursorline     = true
+
+-- Wrap text
+vim.opt.wrap           = true
+
+-- Do not show cursor position separated by comma
+vim.opt.ruler          = false
+
+-- Do not keep search results highlighted
+-- and highlight as typing
+vim.opt.hlsearch       = false
+vim.opt.incsearch      = true
+
+vim.opt.smartindent    = true
+vim.opt.autoindent     = true
+
+-- Ignore case in general, but become case-sensitive
+-- when uppercase is present
+vim.opt.ignorecase     = true
+vim.opt.smartcase      = true
+
+-- Show line number and relative line number
+vim.opt.number         = true
+vim.opt.relativenumber = true
+
+-- options.matching pairs of characters and highlight matching brackets
+vim.opt.matchpairs:append("<:>")
+
+-- Break line at predefined characters
+vim.opt.linebreak = true
+
+-- Split window below/right when creating horizontal/vertical windows
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Height of cmdline space
+vim.opt.cmdheight = 1
+
+-- Time in milliseconds to wait for a mapped sequence to complete,
+-- see https://unix.stackexchange.com/q/36882/221410 for more info
+vim.opt.timeoutlen = 500
+vim.opt.updatetime = 200 -- For CursorHold events
+
+-- General tab settings
+vim.opt.tabstop = 4      -- number of visual spaces per TAB
+vim.opt.softtabstop = 4  -- number of spaces in tab when editing
+vim.opt.shiftwidth = 4   -- number of spaces to use for autoindent
+vim.opt.expandtab = true -- expand tab to spaces so that tabs are spaces
+
+-- List all matches and complete till longest common string
+-- options.wildmode=list:longest
+
+-- Minimum lines to keep above and below cursor when scrolling
+vim.opt.scrolloff = 1
+vim.opt.sidescrolloff = 1
+
+-- Disable showing current mode on command line since
+-- statusline plugins can show it.
+vim.opt.showmode = false
+
+-- Ask for confirmation when handling unsaved or read-only files
+vim.opt.confirm = true
+
+-- Clipboard settings, always use clipboard for all delete, yank, change, put
+-- operation, see https://stackoverflow.com/q/30691466/6064933
+vim.opt.clipboard:append("unnamedplus")
+
+-- Ignore certain files and folders when globing
+vim.opt.wildignore:append("*.o,*.obj,*.dylib,*.bin,*.dll,*.exe")
+vim.opt.wildignore:append("*/.git/*,*/.svn/*,*/__pycache__/*,*/build/**")
+vim.opt.wildignore:append("*.jpg,*.png,*.jpeg,*.bmp,*.gif,*.tiff,*.svg,*.ico")
+vim.opt.wildignore:append("*.pyc,*.pkl")
+vim.opt.wildignore:append("*.DS_Store")
+vim.opt.wildignore:append("*.aux,*.bbl,*.blg,*.brf,*.fls,*.fdb_latexmk,*.synctex.gz,*.xdv")
+vim.opt.wildignorecase = true -- ignore file and dir name cases in cmd-completion
+
+-- Persistent undo even after you close a file and re-open it
+vim.opt.undofile = true
+
+-- Disable swap file
+vim.opt.swapfile = false
+
+-- Create backup for files
+vim.opt.backup = false
+
+-- options.undo directory
+vim.g.undodir = vim.fn.expand(vim.fn.stdpath('data') .. '/undo')
+
+-- Do not use visual and errorbells
+vim.opt.visualbell = false
+vim.opt.errorbells = false
+
+-- The number of command and search history to keep
+vim.opt.history = 500
+
+-- Use mouse to select and resize windows, etc.
+vim.opt.mouse = "a"          -- Enable mouse in several mode
+vim.opt.mousemodel = "popup" -- options.the behaviour of mouse
+vim.opt.mousescroll = "ver:3,hor:10"
+
+-- Do not show "match xx of xx-- and other messages during auto-completion
+vim.opt.shortmess:append("c")
+-- Do not show search match count on bottom right
+vim.opt.shortmess:append("S")
+-- Disable showing intro message (:intro)
+vim.opt.shortmess:append("I")
+
+-- Maximum number of items to show in popup menu
+vim.opt.pumheight = 15
+
+-- Virtual edit is useful for visual block edit
+vim.opt.virtualedit = "block"
+
+-- Tilde (~) is an operator, thus must be followed by motions like `e` or `w`.
+vim.opt.tildeop = true
+
+-- Use rg for 'greping'
+if vim.fn.executable("rg") then
+    vim.opt.grepprg = [[rg --vimgrep --no-heading --smart-case]]
+    vim.opt.grepformat = [[%f:%l:%c:%m]]
+end
+
+-- Enable true color support. Do not options.this option if your terminal does not
+-- support true colors! For a comprehensive list of terminals supporting true
+-- colors, see https://github.com/termstandard/colors and https://gist.github.com/XVilka/8346728.
+vim.opt.termguicolors = true
+
+-- Display column with signs (for things like gitsigns)
+vim.opt.signcolumn = "yes:1"
