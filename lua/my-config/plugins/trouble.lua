e421db617cb1bef54b004dce5819f11a8c584063
natan-alex <natancarmo321@gmail.com>
2023-09-29 07:59:27 -0300

    change trouble mappings and add a new one

--- lua/my-config/plugins/trouble.lua
+++ lua/my-config/plugins/trouble.lua
@@ -1,8 +1,13 @@
+local trouble = require("trouble")
+
 local map = require("my-config.utils.mappings").map
 local mapping_options = { silent = true, noremap = true, }
 
-map("n", "gR"        , "<CMD>TroubleToggle lsp_references<CR>",       mapping_options, "Trouble lsp references")
-map("n", "<Leader>dl", "<CMD>TroubleToggle loclist<CR>",              mapping_options, "Trouble loclist")
-map("n", "<Leader>dq", "<CMD>TroubleToggle quickfix<CR>",             mapping_options, "Trouble quickfix")
-map("n", "<Leader>dd", "<CMD>TroubleToggle document_diagnostics<CR>", mapping_options, "Trouble document diagnostics")
-map("n", "<Leader>do", "<CMD>TroubleToggle<CR>",                      mapping_options, "Trouble toggle")
+map("n", "gR", function() trouble.open("lsp_references") end, mapping_options, "Trouble toggle lsp references")
+map("n", "<leader>xx", function() trouble.open() end, mapping_options, "Trouble toggle")
+map("n", "<leader>xw", function() trouble.open("workspace_diagnostics") end, mapping_options,
+    "Trouble toggle workspace diagnostics")
+map("n", "<leader>xd", function() trouble.open("document_diagnostics") end, mapping_options,
+    "Trouble toggle document diagnostics")
+map("n", "<leader>xq", function() trouble.open("quickfix") end, mapping_options, "Trouble toggle quickfix list")
+map("n", "<leader>xl", function() trouble.open("loclist") end, mapping_options, "Trouble toggle loclist")
