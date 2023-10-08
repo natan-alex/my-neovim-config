7ffae3def78847370743e7f9f104d48bd74ce79b
natan-alex <natancarmo321@gmail.com>
2023-10-08 16:15:28 -0300

    change mapping function

--- lua/my-config/plugins/nvim-hlslens.lua
+++ lua/my-config/plugins/nvim-hlslens.lua
@@ -4,16 +4,15 @@ return {
     config = function()
         require("hlslens").setup()
 
-        local mapping_options = { silent = true, noremap = true }
+        local map = require("my-config.utils.mappings").map
+        local mapping_options = { silent = true, noremap = true, nowait = true }
 
-        vim.api.nvim_set_keymap('n', 'n',
-            [[<CMD>execute('normal! ' . v:count1 . 'n')<CR><CMD>lua require('hlslens').start()<CR>]], mapping_options)
+        map("n", "<Leader>hh", "<CMD>noh<CR>", mapping_options)
 
-        vim.api.nvim_set_keymap('n', 'N',
-            [[<CMD>execute('normal! ' . v:count1 . 'N')<CR><CMD>lua require('hlslens').start()<CR>]], mapping_options)
+        map("n", "*", [[*<CMD>lua require("hlslens").start()<CR>]], mapping_options)
 
-        vim.api.nvim_set_keymap('n', '*', [[*<CMD>lua require('hlslens').start()<CR>]], mapping_options)
+        map("n", "n", [[<CMD>execute("normal! " . v:count1 . "n")<CR><CMD>lua require("hlslens").start()<CR>]], mapping_options)
 
-        vim.api.nvim_set_keymap('n', '<Leader>hh', '<CMD>noh<CR>', mapping_options)
+        map("n", "N", [[<CMD>execute("normal! " . v:count1 . "N")<CR><CMD>lua require("hlslens").start()<CR>]], mapping_options)
     end
 }
