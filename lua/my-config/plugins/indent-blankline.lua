a5c716df8056fe48436f807d278bdddc5e3ba59d
natan-alex <natancarmo321@gmail.com>
2023-10-10 19:07:05 -0300

    update indent blank line setup call

--- lua/my-config/plugins/indent-blankline.lua
+++ lua/my-config/plugins/indent-blankline.lua
@@ -1,5 +1,5 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     event = "VimEnter",
-    config = function() require("indent_blankline").setup() end,
+    config = function() require("ibl").setup() end,
 }
