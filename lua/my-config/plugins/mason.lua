718a32591b53ab5695dd05e3ad5bc55da3d94297
natan-alex <natancarmo321@gmail.com>
2023-12-30 20:58:41 -0300

    split mason and mason-lspconfig

--- lua/my-config/plugins/mason.lua
+++ lua/my-config/plugins/mason.lua
@@ -1,42 +1,15 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
+	"williamboman/mason.nvim",
+	cmd = "Mason",
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
 				},
-			})
-		end,
-	},
-
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
-		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local lspconfig = require("lspconfig")
-
-			mason_lspconfig.setup()
-
-			local capabilities = cmp_nvim_lsp.default_capabilities()
-
-			mason_lspconfig.setup_handlers({
-				-- The first entry (without a key) will be the default handler
-				-- and will be called for each installed server that doesn't have
-				-- a dedicated handler.
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-			})
-		end,
-	},
+			},
+		})
+	end,
 }
