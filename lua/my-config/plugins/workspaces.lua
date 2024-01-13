d4a5865bf2a930ff566165f9fd9f65d85fbfeaa9
natan-alex <natancarmo321@gmail.com>
2024-01-13 00:09:05 -0300

    add workspaces plugin to switch between projects

---
+++ lua/my-config/plugins/workspaces.lua
@@ -0,0 +1,23 @@
+return {
+    "natecraddock/workspaces.nvim",
+    after = { "nvim-telescope/telescope.nvim" },
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    keys = {
+        { "<C-p>", "<CMD>Telescope workspaces<CR>", mode = "n" },
+        { "<Leader>wa", "<CMD>WorkspacesAdd<CR>", mode = "n" },
+        { "<Leader>wd", "<CMD>WorkspacesRemove<CR>", mode = "n" },
+    },
+    config = function()
+        require("workspaces").setup({
+            hooks = {
+                open_pre = "SessionSave",
+                open = {
+                    "silent %bdelete!",
+                    "SessionRestore",
+                },
+            },
+        })
+
+        require("telescope").load_extension("workspaces")
+    end,
+}
