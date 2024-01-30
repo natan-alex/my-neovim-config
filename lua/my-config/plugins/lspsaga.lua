9ad440dbcfc033a2d85632c14ca9dc600767fbe2
natan-alex <natancarmo321@gmail.com>
2024-01-30 20:57:56 -0300

    add lspsaga plugin and configure lsp mappings

---
+++ lua/my-config/plugins/lspsaga.lua
@@ -0,0 +1,43 @@
+return {
+	"nvimdev/lspsaga.nvim",
+	event = "LspAttach",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{ "K", "<CMD>Lspsaga hover_doc<CR>", mode = "n", desc = "Lspsaga hover" },
+		{ "gl", "<CMD>Lspsaga show_line_diagnostics<CR>", mode = "n", desc = "Lspsaga show line diagnostics" },
+		-- { "gd", "<CMD>Lspsaga goto_definition<CR>", mode = "n", desc = "Lspsaga go to definition" },
+		-- { "gD", "<CMD>Lspsaga peek_definition<CR>", mode = "n", desc = "Lspsaga peek definition" },
+		-- { "gt", "<CMD>Lspsaga goto_type_definition<CR>", mode = "n", desc = "Lspsaga go to type definition" },
+		-- { "gT", "<CMD>Lspsaga peek_type_definition<CR>", mode = "n", desc = "Lspsaga peek type definition" },
+		{ "gr", "<CMD>Lspsaga finder<CR>", mode = "n", desc = "Lspsaga open finder" },
+		{ "[d", "<CMD>Lspsaga diagnostic_jump_prev<CR>", mode = "n", desc = "Lspsaga go to previous diagnostic" },
+		{ "]d", "<CMD>Lspsaga diagnostic_jump_next<CR>", mode = "n", desc = "Lspsaga go to next diagnostic" },
+		{ "<Leader>rn", "<CMD>Lspsaga rename<CR>", mode = "n", desc = "Lspsaga rename" },
+		{ "<Leader>ca", "<CMD>Lspsaga code_action<CR>", mode = "n", desc = "Lspsaga show code actions" },
+		{ "<Leader>o", "<CMD>Lspsaga outline<CR>", mode = "n", desc = "Lspsaga open outline" },
+	},
+	config = function()
+		require("lspsaga").setup({
+			code_action = {
+				extend_gitsigns = true,
+			},
+			finder = {
+				keys = {
+					close = "<C-c>",
+				},
+			},
+            outline = {
+                close_after_jump = true,
+            },
+            rename = {
+                in_select = false,
+                keys = {
+					close = "<C-c>",
+                },
+            },
+		})
+	end,
+}
--- lua/my-config/plugins/trouble.lua
+++ lua/my-config/plugins/trouble.lua
@@ -3,21 +3,25 @@ return {
 	cmd = { "TroubleToggle", "Trouble" },
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	keys = {
-		{ "gR", "<CMD>Trouble lsp_references<CR>", mode = "n", desc = "Trouble toggle lsp references" },
+		{ "gR", "<CMD>Trouble lsp_references<CR>", mode = "n", desc = "Trouble show lsp references" },
+		-- { "gd", "<CMD>Trouble lsp_definitions<CR>", mode = "n", desc = "Trouble show lsp definitions" },
+		-- { "gi", "<CMD>Trouble lsp_implementations<CR>", mode = "n", desc = "Trouble show lsp implementations" },
+		-- { "gt", "<CMD>Trouble lsp_type_definitions<CR>", mode = "n", desc = "Trouble show lsp type definitions" },
+
 		{ "<leader>xx", "<CMD>TroubleToggle<CR>", mode = "n", desc = "Trouble toggle" },
 		{
 			"<leader>xw",
 			"<CMD>Trouble workspace_diagnostics<CR>",
 			mode = "n",
-			desc = "Trouble toggle workspace diagnostics",
+			desc = "Trouble show workspace diagnostics",
 		},
 		{
 			"<leader>xd",
 			"<CMD>Trouble document_diagnostics<CR>",
 			mode = "n",
-			desc = "Trouble toggle document diagnostics",
+			desc = "Trouble show document diagnostics",
 		},
-		{ "<leader>xq", "<CMD>Trouble quickfix<CR>", mode = "n", desc = "Trouble toggle quickfix list" },
-		{ "<leader>xl", "<CMD>Trouble loclist<CR>", mode = "n", desc = "Trouble toggle loclist" },
+		{ "<leader>xq", "<CMD>Trouble quickfix<CR>", mode = "n", desc = "Trouble show quickfix list" },
+		{ "<leader>xl", "<CMD>Trouble loclist<CR>", mode = "n", desc = "Trouble show loclist" },
 	},
 }
