6c6631fbb35bbdf8d5dfac0728f1df592afbecfe
natan-alex <natancarmo321@gmail.com>
2024-03-04 22:46:19 -0300

    remove some bufferline options

--- lua/my-config/plugins/bufferline.lua
+++ lua/my-config/plugins/bufferline.lua
@@ -29,15 +29,7 @@ return {
 				close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 				right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 				left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-				buffer_close_icon = "",
-				modified_icon = "●",
-				close_icon = "",
-				left_trunc_marker = "",
-				right_trunc_marker = "",
-				max_name_length = 25,
-				max_prefix_length = 18, -- prefix used when a buffer is de-duplicated
 				truncate_names = true, -- whether or not tab names should be truncated
-				tab_size = 20,
 				diagnostics = "nvim_lsp",
 				diagnostics_update_in_insert = false,
 				diagnostics_indicator = function(count, _, _, _)
@@ -56,13 +48,11 @@ return {
 				show_buffer_close_icons = false,
 				show_close_icon = false,
 				show_tab_indicators = true,
-				show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
 				persist_buffer_sort = false,
 				separator_style = "thin",
 				enforce_regular_tabs = true,
 				always_show_bufferline = false,
 				hover = { enabled = false },
-				sort_by = "id",
 			},
 		})
 	end,
