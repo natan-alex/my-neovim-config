418f90f7fe0ff56d1a2824536336206e0047f6dc
natan-alex <natancarmo321@gmail.com>
2024-03-04 22:51:07 -0300

    use g* instead h* for gitsigns

--- lua/my-config/plugins/gitsigns.lua
+++ lua/my-config/plugins/gitsigns.lua
@@ -18,26 +18,26 @@ return {
 				local map = require("my-config.utils.mappings").map
 				local mapping_options = { silent = true, noremap = true, buffer = buffer }
 
-				map("n", "<Leader>hS", gitsigns.stage_buffer, mapping_options, "Gitsigns stage buffer")
-				map("n", "<Leader>hu", gitsigns.undo_stage_hunk, mapping_options, "Gitsigns undo stage hunk")
-				map("n", "<Leader>hR", gitsigns.reset_buffer, mapping_options, "Gitsigns reset buffer")
-				map("n", "<Leader>hp", gitsigns.preview_hunk, mapping_options, "Gitsigns preview hunk")
-				map("n", "<Leader>hd", gitsigns.diffthis, mapping_options, "Gitsigns diff this")
-				map("n", "<Leader>hD", gitsigns.toggle_deleted, mapping_options, "Gitsigns toggle deleted")
-				map("n", "<Leader>hs", gitsigns.stage_hunk, mapping_options, "Gitsigns stage hunk")
-				map("n", "<Leader>hr", gitsigns.reset_hunk, mapping_options, "Gitsigns reset hunk")
+				map("n", "<Leader>gS", gitsigns.stage_buffer, mapping_options, "Gitsigns stage buffer")
+				map("n", "<Leader>gu", gitsigns.undo_stage_hunk, mapping_options, "Gitsigns undo stage hunk")
+				map("n", "<Leader>gR", gitsigns.reset_buffer, mapping_options, "Gitsigns reset buffer")
+				map("n", "<Leader>gp", gitsigns.preview_hunk, mapping_options, "Gitsigns preview hunk")
+				map("n", "<Leader>gd", gitsigns.diffthis, mapping_options, "Gitsigns diff this")
+				map("n", "<Leader>gD", gitsigns.toggle_deleted, mapping_options, "Gitsigns toggle deleted")
+				map("n", "<Leader>gs", gitsigns.stage_hunk, mapping_options, "Gitsigns stage hunk")
+				map("n", "<Leader>gr", gitsigns.reset_hunk, mapping_options, "Gitsigns reset hunk")
 
 				map(
 					"n",
-					"<Leader>hl",
+					"<Leader>gl",
 					gitsigns.toggle_current_line_blame,
 					mapping_options,
 					"Gitsigns toggle current line blame"
 				)
 
-				map("n", "]h", function()
+				map("n", "]g", function()
 					if vim.wo.diff then
-						return "]h"
+						return "]g"
 					end
 					vim.schedule(function()
 						gitsigns.next_hunk()
@@ -45,9 +45,9 @@ return {
 					return "<Ignore>"
 				end, mapping_options, "Gitsigns go to next hunk")
 
-				map("n", "[h", function()
+				map("n", "[g", function()
 					if vim.wo.diff then
-						return "[h"
+						return "[g"
 					end
 					vim.schedule(function()
 						gitsigns.prev_hunk()
