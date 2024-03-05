7d54c283a8e7c706ff3c6f6df4446ff66579bdaf
natan-alex <natancarmo321@gmail.com>
2024-03-04 23:16:22 -0300

    add mappings for some lspfuzzy commands

--- lua/my-config/plugins/nvim-lspfuzzy.lua
+++ lua/my-config/plugins/nvim-lspfuzzy.lua
@@ -5,6 +5,20 @@ return {
 		{ "junegunn/fzf" },
 		{ "junegunn/fzf.vim" }, -- to enable preview (optional)
 	},
+    keys = {
+		{
+			"<Leader>fz",
+			"<CMD>LspDiagnosticsAll<CR>",
+			mode = "n",
+			desc = "Lsp Fuzzy show all diagnostics",
+		},
+		{
+			"<Leader>f.",
+			"<CMD>LspDiagnostics 0<CR>",
+			mode = "n",
+			desc = "Lsp Fuzzy show diagnostics for current buffer",
+		},
+    },
 	config = function()
 		require("lspfuzzy").setup({})
 	end,
