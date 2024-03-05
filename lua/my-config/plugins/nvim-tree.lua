cb8d2373f03690e63001a86eb9ef8d25d64479f1
natan-alex <natancarmo321@gmail.com>
2024-03-05 10:04:54 -0300

    change and add nvim-tree options

--- lua/my-config/plugins/nvim-tree.lua
+++ lua/my-config/plugins/nvim-tree.lua
@@ -1,57 +1,76 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	keys = {
-		{ "<Leader>e", "<CMD>NvimTreeToggle<CR>", desc = "NvimTree toggle" },
-	},
-	config = function()
-		local nvim_tree = require("nvim-tree")
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+        { "<Leader>e", "<CMD>NvimTreeToggle<CR>", desc = "NvimTree toggle" },
+    },
+    config = function()
+        local nvim_tree = require("nvim-tree")
 
-		nvim_tree.setup({
-			sort_by = "name",
-			disable_netrw = true,
-			hijack_cursor = true,
-			respect_buf_cwd = true,
-			sync_root_with_cwd = true,
-			modified = {
-				enable = true,
-				show_on_dirs = true,
-			},
-			filesystem_watchers = {
-				enable = true,
-			},
-			renderer = {
-				highlight_opened_files = "all",
-				indent_markers = {
-					enable = true,
-				},
-			},
-			diagnostics = {
-				enable = true,
-				show_on_dirs = true,
-			},
-			actions = {
-				open_file = {
-					quit_on_open = true,
-				},
-			},
-			update_focused_file = {
-				enable = true,
-				update_root = false,
-			},
-			view = {
-				adaptive_size = true,
-			},
-			on_attach = function(buffer)
-				local api = require("nvim-tree.api")
+        nvim_tree.setup({
+            disable_netrw = true,
+            hijack_cursor = true,
+            select_prompts = true,
+            respect_buf_cwd = true,
+            sync_root_with_cwd = true,
+            hijack_unnamed_buffer_when_opening = true,
 
-				local map = require("my-config.utils.mappings").map
-				local options = { noremap = true, silent = true, nowait = true, buffer = buffer }
+            update_focused_file = {
+                enable = true,
+            },
 
-				api.config.mappings.default_on_attach(buffer)
+            view = {
+                cursorline = true,
+                debounce_delay = 100,
+                centralize_selection = true,
+            },
 
-				map("n", "?", api.tree.toggle_help, options, "NvimTree show help")
-				map("n", "C", api.tree.change_root_to_node, options, "NvimTree change cwd")
-			end,
-		})
-	end,
+            renderer = {
+                highlight_git = "all",
+                highlight_modified = "all",
+                highlight_clipboard = "all",
+                highlight_diagnostics = "all",
+                highlight_opened_files = "name",
+                icons = {
+                    web_devicons = {
+                        folder = {
+                            enable = true
+                        },
+                    },
+                },
+            },
+
+            diagnostics = {
+                enable = true,
+                show_on_dirs = true,
+                debounce_delay = 100,
+            },
+
+            modified = {
+                enable = true,
+            },
+
+            filters = {
+                git_ignored = false,
+                dotfiles = true,
+            },
+
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                },
+            },
+
+            on_attach = function(buffer)
+                local api = require("nvim-tree.api")
+
+                local map = require("my-config.utils.mappings").map
+                local options = { noremap = true, silent = true, nowait = true, buffer = buffer }
+
+                api.config.mappings.default_on_attach(buffer)
+
+                map("n", "?", api.tree.toggle_help, options, "NvimTree show help")
+                map("n", "C", api.tree.change_root_to_node, options, "NvimTree change cwd")
+            end,
+        })
+    end,
 }
