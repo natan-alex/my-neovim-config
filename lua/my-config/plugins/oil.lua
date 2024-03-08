1b2cc9898ded1e422f8139d8dba8b7e13377ab1f
natan-alex <natancarmo321@gmail.com>
2024-03-07 22:19:28 -0300

    change oil trigger and add more mappings

--- lua/my-config/plugins/oil.lua
+++ lua/my-config/plugins/oil.lua
@@ -1,10 +1,16 @@
 return {
-	"stevearc/oil.nvim",
-	keys = {
-		{ "-", "<CMD>Oil<CR>", desc = "Oil Open parent directory" },
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({})
-	end,
+    "stevearc/oil.nvim",
+    keys = {
+        { "<Leader>E", "<CMD>Oil<CR>", desc = "Oil Open parent directory" },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("oil").setup({
+            keymaps = {
+                ["?"] = "actions.show_help",
+                [">"] = "actions.select",
+                ["<"] = "actions.parent",
+            },
+        })
+    end,
 }
