9cdbf69c877d79fc867696eccfccaad57193654a
natan-alex <natancarmo321@gmail.com>
2024-03-14 11:53:27 -0300

    add todo-comments plugin

---
+++ lua/my-config/plugins/todo-comments.lua
@@ -0,0 +1,32 @@
+return {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        {
+            "[t",
+            function()
+                require("todo-comments").jump_prev()
+            end,
+            mode = "n",
+            desc = "Go to previous todo comment"
+        },
+        {
+            "]t",
+            function()
+                require("todo-comments").jump_next()
+            end,
+            mode = "n",
+            desc = "Go to next todo comment"
+        },
+        {
+            "<Leader>tt",
+            "<CMD>TodoTrouble<CR>",
+            mode = "n",
+            desc = "Toggle todo list using Trouble"
+        },
+    },
+    config = function()
+        require("todo-comments").setup({})
+    end
+}
