686f94fc9e147b434ec7288a533aef389410d2e9
natan-alex <natancarmo321@gmail.com>
2024-05-09 20:48:22 -0300

    get eslint source from none ls extras

--- lua/my-config/plugins/none-ls.lua
+++ lua/my-config/plugins/none-ls.lua
@@ -1,15 +1,16 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvimtools/none-ls-extras.nvim" },
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.code_actions.eslint_d,
-			},
-		})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettierd,
+                require("none-ls.diagnostics.eslint_d"),
+                require("none-ls.code_actions.eslint_d"),
+            },
+        })
+    end,
 }
