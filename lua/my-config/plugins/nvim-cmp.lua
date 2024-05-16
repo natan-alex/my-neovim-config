0f3b634dfc880e834fc9dde36f68cd3da30da60b
natan-alex <natancarmo321@gmail.com>
2024-05-16 19:23:35 -0300

    improving nvim-cmp related config

--- lua/my-config/plugins/nvim-cmp.lua
+++ lua/my-config/plugins/nvim-cmp.lua
@@ -1,68 +1,97 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-emoji" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "hrsh7th/cmp-cmdline" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-emoji" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "hrsh7th/cmp-cmdline" },
+        { "onsails/lspkind.nvim" },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "L3MON4D3/LuaSnip",        build = "make install_jsregexp" },
+    },
+    config = function()
+        local lspkind = require("lspkind")
+        lspkind.init({})
 
-		cmp.setup({
-			sources = cmp.config.sources({
-				{ name = "luasnip" },
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-				{ name = "emoji" },
-				{ name = "treesitter" },
-			}),
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = {
-				["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-				["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-				["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-				["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-				["<C-l>"] = cmp.mapping(cmp.mapping.complete(), { "i" }),
-				["<Up>"] = cmp.mapping.scroll_docs(-1),
-				["<Down>"] = cmp.mapping.scroll_docs(1),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<C-c>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm(),
-				["<Tab>"] = cmp.mapping(function(fallback)
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        cmp.setup({
+            sources = cmp.config.sources({
+                { name = "luasnip" },
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+                { name = "emoji" },
+                { name = "treesitter" },
+            }),
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = 'symbol_text',
+                    maxwidth = 50,
+                    ellipsis_char = '...',
+                    show_labelDetails = true,
+                })
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-c>"] = cmp.mapping.abort(),
+                ["<C-y>"] = cmp.mapping.complete(),
+                ["<C-p>"] = cmp.mapping.select_prev_item({
+                    behavior = cmp.SelectBehavior.Insert
+                }),
+                ["<C-n>"] = cmp.mapping.select_next_item({
+                    behavior = cmp.SelectBehavior.Insert
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
                     if cmp.visible() then
-                        cmp.confirm({ select = true })
+                        local entry = cmp.get_selected_entry()
+                        if not entry then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        end
+                        cmp.confirm()
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
-			},
-		})
+                end, { "i", "s", "c", }),
+            }),
+        })
 
-		-- Set configuration for specific filetype.
-		cmp.setup.filetype("gitcommit", {
-			sources = cmp.config.sources({
-				{ name = "git" },
-				{ name = "buffer" },
-			}),
-		})
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
 
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = { { name = "buffer" } },
-		})
-	end,
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                    { name = 'cmdline' }
+                }),
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
+
+
+        -- insert `(` after select function or method item
+        local module_found, cmp_autopairs = pcall(require('nvim-autopairs.completion.cmp'))
+
+        if module_found then
+            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+        end
+    end,
 }
