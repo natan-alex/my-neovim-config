8770e41b69f40093bd7a69382dd63d43e2421257
natan-alex <natancarmo321@gmail.com>
2024-05-16 15:53:35 -0300

    add build prop to luasnip deps table

--- lua/my-config/plugins/nvim-cmp.lua
+++ lua/my-config/plugins/nvim-cmp.lua
@@ -7,7 +7,7 @@ return {
 		{ "hrsh7th/cmp-path" },
 		{ "hrsh7th/cmp-cmdline" },
 		{ "saadparwaiz1/cmp_luasnip" },
-		{ "L3MON4D3/LuaSnip" },
+		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
 	},
 	config = function()
 		local cmp = require("cmp")
