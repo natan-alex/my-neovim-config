c946e5b04a185c96e662282e49526dafd9700426
natan-alex <natancarmo321@gmail.com>
2024-07-15 10:55:16 -0300

    make tab insert the first entry

--- lua/my-config/plugins/nvim-cmp.lua
+++ lua/my-config/plugins/nvim-cmp.lua
@@ -57,7 +57,9 @@ return {
                     if cmp.visible() then
                         local entry = cmp.get_selected_entry()
                         if not entry then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            cmp.select_next_item({
+                                behavior = cmp.SelectBehavior.Insert
+                            })
                         end
                         cmp.confirm()
                     else
