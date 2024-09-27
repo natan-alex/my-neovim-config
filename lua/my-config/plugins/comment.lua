b922bd686468b68559b95168426bf7a1f7e602c6
natan-alex <natancarmo321@gmail.com>
2024-09-27 19:55:25 -0300

    remove custom mappings for comment

--- lua/my-config/plugins/comment.lua
+++ lua/my-config/plugins/comment.lua
@@ -22,28 +22,6 @@ return {
 				---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
 				extended = false,
 			},
-			toggler = {
-				---Line-comment toggle keymap
-				line = "<Leader>cc",
-				---Block-comment toggle keymap
-				block = "<Leader>cb",
-			},
-			---LHS of operator-pending mappings in NORMAL and VISUAL mode
-			opleader = {
-				---Line-comment keymap
-				line = "<Leader>c",
-				---Block-comment keymap
-				block = "<Leader>b",
-			},
-			---LHS of extra mappings
-			extra = {
-				---Add comment on the line above
-				above = "<Leader>cO",
-				---Add comment on the line below
-				below = "<Leader>co",
-				---Add comment at the end of line
-				eol = "<Leader>cA",
-			},
 		})
 	end,
 }
