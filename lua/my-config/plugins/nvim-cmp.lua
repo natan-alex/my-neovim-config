4e5c1336f32664193382ff6b9e5e473fadf31f1b
natan-alex <natancarmo321@gmail.com>
2024-09-27 20:03:21 -0300

    format file

--- lua/my-config/plugins/nvim-cmp.lua
+++ lua/my-config/plugins/nvim-cmp.lua
@@ -8,7 +8,7 @@ return {
         { "hrsh7th/cmp-cmdline" },
         { "onsails/lspkind.nvim" },
         { "saadparwaiz1/cmp_luasnip" },
-        { "L3MON4D3/LuaSnip", build = "make" },
+        { "L3MON4D3/LuaSnip",        build = "make" },
     },
     config = function()
         local lspkind = require("lspkind")
@@ -37,20 +37,20 @@ return {
             },
             formatting = {
                 format = lspkind.cmp_format({
-                    mode = 'symbol_text',
+                    mode = "symbol_text",
                     maxwidth = 50,
-                    ellipsis_char = '...',
+                    ellipsis_char = "...",
                     show_labelDetails = true,
-                })
+                }),
             },
             mapping = cmp.mapping.preset.insert({
                 ["<C-c>"] = cmp.mapping.abort(),
                 ["<C-y>"] = cmp.mapping.complete(),
                 ["<C-p>"] = cmp.mapping.select_prev_item({
-                    behavior = cmp.SelectBehavior.Insert
+                    behavior = cmp.SelectBehavior.Insert,
                 }),
                 ["<C-n>"] = cmp.mapping.select_next_item({
-                    behavior = cmp.SelectBehavior.Insert
+                    behavior = cmp.SelectBehavior.Insert,
                 }),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
                 ["<Tab>"] = cmp.mapping(function(fallback)
@@ -59,42 +59,41 @@ return {
                         local entry = cmp.get_selected_entry()
                         if not entry then
                             cmp.select_next_item({
-                                behavior = cmp.SelectBehavior.Insert
+                                behavior = cmp.SelectBehavior.Insert,
                             })
                         end
                         cmp.confirm()
                     else
                         fallback()
                     end
-                end, { "i", "s", "c", }),
+                end, { "i", "s", "c" }),
             }),
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
+        cmp.setup.cmdline({ "/", "?" }, {
             mapping = cmp.mapping.preset.cmdline(),
             sources = {
-                { name = 'buffer' }
-            }
+                { name = "buffer" },
+            },
         })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
+        cmp.setup.cmdline(":", {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({
-                { name = 'path' }
+                { name = "path" },
             }, {
-                    { name = 'cmdline' }
-                }),
-            matching = { disallow_symbol_nonprefix_matching = false }
+                { name = "cmdline" },
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false },
         })
 
-
         -- insert `(` after select function or method item
-        local module_found, cmp_autopairs = pcall(require('nvim-autopairs.completion.cmp'))
+        local module_found, cmp_autopairs = pcall(require("nvim-autopairs.completion.cmp"))
 
         if module_found then
-            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end
     end,
 }
