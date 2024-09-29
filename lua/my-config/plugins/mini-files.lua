349d0ed156830f485724f479aff887469f637b67
natan-alex <natancarmo321@gmail.com>
2024-09-29 14:51:53 -0300

    change mini.files mappings

--- lua/my-config/plugins/mini-files.lua
+++ lua/my-config/plugins/mini-files.lua
@@ -1,17 +1,35 @@
 return {
-	"echasnovski/mini.files",
-	version = "*",
-	keys = {
-		{
-			"<Leader>e",
-			function()
-				require("mini.files").open()
-			end,
-			mode = "n",
-			desc = "Open Mini.Files",
-		},
-	},
-	config = function()
-		require("mini.files").setup({})
-	end,
+    "echasnovski/mini.files",
+    version = "*",
+    keys = {
+        {
+            "<Leader>e",
+            function()
+                require("mini.files").open()
+            end,
+            mode = "n",
+            desc = "Open Mini.Files",
+        },
+    },
+    config = function()
+        require("mini.files").setup({
+            -- Module mappings created only inside explorer.
+            -- Use `''` (empty string) to not create one.
+            mappings = {
+                close = "q",
+                go_in = ">",
+                go_in_plus = "<CR>",
+                go_out = "<",
+                go_out_plus = "-",
+                mark_goto = "'",
+                mark_set = "m",
+                reset = "<BS>",
+                reveal_cwd = "@",
+                show_help = "g?",
+                synchronize = "=",
+                trim_left = "<C-h>",
+                trim_right = "<C-l>",
+            },
+        })
+    end,
 }
