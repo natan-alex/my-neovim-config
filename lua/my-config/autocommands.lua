c521e4413ca812144bdafd746d6c67cf974a74bc
natan-alex <natancarmo321@gmail.com>
2024-12-18 15:03:02 -0300

    refactor autocommands and remove unnecessary ones

--- lua/my-config/autocommands.lua
+++ lua/my-config/autocommands.lua
@@ -1,45 +1,27 @@
 -- Highlight yanked region, see `:h lua-highlight`
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  pattern = "*",
-  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-on-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ timeout = 100 })
-  end,
-})
-
--- Check for file changes
-vim.api.nvim_create_autocmd({ "FocusGained", "CursorHold" }, {
-  pattern = "*",
-  group = vim.api.nvim_create_augroup("auto_read", { clear = true }),
-  callback = function()
-    if vim.fn.getcmdwintype() == "" then
-      vim.cmd("checktime")
-    end
+    vim.highlight.on_yank()
   end,
 })
 
 -- Handle large files
 -- ref: https://vi.stackexchange.com/a/169/15292
 vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = "*",
-  group = vim.api.nvim_create_augroup("large_file", { clear = true }),
+  group = vim.api.nvim_create_augroup("handle-large-files", { clear = true }),
   callback = function()
-    local file = vim.fn.expand("<afile>")
+    local maximum_file_size = 10 * 1024 * 1024 -- 10MB
+    local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
 
-    local large_file_size = 10485760 -- 10MB
-
-    local file_size = vim.fn.getfsize(file)
-
-    if file_size > large_file_size or file_size == -2 then
+    if file_size > maximum_file_size then
+      vim.cmd("syntax off")
       vim.opt.eventignore:append("all")
       vim.opt.cursorline = false
       vim.opt.relativenumber = false
       vim.opt.swapfile = false
       vim.opt.undolevels = 5
       vim.opt.bufhidden = "unload"
-    else
-      vim.opt.eventignore:remove("all")
-      vim.opt.relativenumber = true
     end
   end,
 })
