10e054e60d2d5022451b293608d12b5a78ea6877
natan-alex <natancarmo321@gmail.com>
2025-02-27 20:51:48 -0300

    simplify statusline

--- lua/my-config/plugins/lualine.lua
+++ lua/my-config/plugins/lualine.lua
@@ -4,14 +4,16 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true,
+        component_separators = { left = "⚡", right = "⚡" },
+        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "diff" },
-        lualine_x = { "filetype" },
-        lualine_y = { "diagnostics" },
-        lualine_z = { "location" },
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "diagnostics", "branch" },
       },
     })
   end,
